4a57821c35b7cb65315cb58a19da742fc45ae178
ismo.aro@gmail.com <ismo.aro@gmail.com>
2010-07-13 13:07:32 +0300

    created and modified acceptance test cases to suite better what we are doing, hopefully everyone else is agrees :)

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,2 @@
-def use_interface(int_alias, phy_int, ip_address, netmask):
+def use_interface(int_alias, phy_int, virtual_interface = None, ip_address = None, netmask = None):
     print 'Hello World!'
