259fb40216a52ed867bc56b5e53183ae5b1ed06e
ismo.aro@gmail.com <ismo.aro@gmail.com>
2010-07-13 13:51:22 +0300

    little bit more modification to acceptance tests. now they should be world class ;)

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,6 @@
-def use_interface(int_alias, phy_int, virtual_interface = None, ip_address = None, netmask = None):
+import socket
+
+def use_interface(int_alias, ifname, virtual_interface = None, ip_address = None, netmask = None):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print s
     print 'Hello World!'
