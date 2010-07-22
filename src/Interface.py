443c57cfbf24d870f56fa0b05929b5cbf474e161
ismo.aro@gmail.com <ismo.aro@gmail.com>
2010-07-22 14:41:15 +0300

    first proto version of create_interface keyword impelemented

--- src/Interface.py
+++ src/Interface.py
@@ -1,8 +1,9 @@
+import os
 class Interface:
     """Interface class for interface handling"""
     
     def create_interface(self, int_alias, ifname, ip_address, netmask):
-        raise NotImplementedError("This is not ready yet!")
+        os.popen("ifconfig "+ifname+":1 "+ip_address+" netmask "+netmask)
 
     def is_interface_up (int_alias):
-        raise Exception, "No code here yet!"
+        raise NotImplementedError("This is not ready yet!")
--- src/__init__.py
+++ src/__init__.py
@@ -5,5 +5,5 @@ def use_interface(int_alias, ifname, ip_address = None, netmask = None, virtual_
     if virtual_interface != False:
         Interface().create_interface(int_alias, ifname, ip_address, netmask)
     else:
-        raise Exception, "No code here yet"
+        raise NotImplementedError("This is not ready yet!")
    
