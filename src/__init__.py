3cc6e6324b35601b1e807cada5bb1587c871ba62
ismo.aro@gmail.com <ismo.aro@gmail.com>
2010-07-22 12:48:10 +0300

    corrected 1 typo

--- src/__init__.py
+++ src/__init__.py
@@ -5,5 +5,5 @@ def use_interface(int_alias, ifname, ip_address = None, netmask = None, virtual_
     if virtual_interface != False:
         interface.create_interface(int_alias, ifname, virtual_interface, ip_address, netmask)
     else:
-        raise Exeption, "No code here yet"
+        raise Exception, "No code here yet"
    
