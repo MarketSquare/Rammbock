4f65efb47b27e37f0699f548d8779af770f86ae2
ismo.aro@gmail.com <ismo.aro@gmail.com>
2010-07-22 12:44:31 +0300

    put test cases fail again and included importing to interface class __init__

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,9 @@
 import socket
+from Interface import Interface
 
-def use_interface(int_alias, ifname, virtual_interface = None, ip_address = None, netmask = None):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print s
-    print 'Hello World!'
+def use_interface(int_alias, ifname, ip_address = None, netmask = None, virtual_interface = False):
+    if virtual_interface != False:
+        interface.create_interface(int_alias, ifname, virtual_interface, ip_address, netmask)
+    else:
+        raise Exeption, "No code here yet"
+   
