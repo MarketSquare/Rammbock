4fbf2e60967b8b38ab60bc754f22654efdd2a788
ismo.aro@gmail.com <ismo.aro@gmail.com>
2010-07-28 13:49:04 +0300

    first version of implementation is ready for Creating Virtual Interface test case

--- src/Interface.py
+++ src/Interface.py
@@ -6,6 +6,20 @@ class Interface:
         os.popen("ifconfig "+ifname+":1 "+ip_address+" netmask "+netmask)
 
     def check_interface (self, ifname):
-        output = os.popen("ifconfig "+ifname+":1")
-        print output
-        raise NotImplementedError("This is not ready yet!")
+        ipaddress= self.get_ip_address(ifname)
+        if ipaddress == None:
+            raise Exception("No interface found!")
+        else:
+            return True
+
+    def get_ip_address(self, ifname):
+        """
+        Returns ip address from local machine. interface name is given as an parameter.
+        get_ip_address | <interface>
+        e.g. get_ip_address | eth0
+        """
+        ipAddressList = os.popen("/sbin/ifconfig "+ifname+" | grep inet | awk '{print $2}' | sed -e s/.*://").readlines()
+        ipAddress = "".join(ipAddressList)
+        ipAddress = ipAddress.strip()
+        print ipAddress
+        return ipAddress 
