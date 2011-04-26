28df0c56edb60ea91b23154c22b9c43ebe692112
Mikko Korpela <mikko.korpela@gmail.com>
2011-04-26 15:33:51 +0300

    add keyword for deleting all created virtual interfaces

--- src/rammbock/Rammbock.py
+++ src/rammbock/Rammbock.py
@@ -5,15 +5,18 @@ from rammbock.Interface import VirtualInterface, PhysicalInterface
 class Rammbock(object):
 
     def __init__(self):
-            self.interfaces = {}
-            self._client = Client()
-            self._server = Server(self.interfaces)
+        self.interfaces = {}
+        self._virtual_interfaces = set()
+        self._client = Client()
+        self._server = Server(self.interfaces)
 
     def create_virtual_interface(self, if_alias, ifname, ip_address, netmask):
         print "create_virtual_interface " + if_alias
         if if_alias in self.interfaces and  self.interfaces[if_alias].ifUp:
             raise Exception('Interface "%s" already exists' % if_alias)
-        self.interfaces[if_alias] = VirtualInterface(if_alias, ifname, ip_address, netmask)
+        interface = VirtualInterface(if_alias, ifname, ip_address, netmask)
+        self.interfaces[if_alias] = interface
+        self._virtual_interfaces.add(interface)
 
     def use_interface(self, if_alias, ifname, ip_address=None, netmask=None):
         print "use_interface " + if_alias
@@ -26,6 +29,11 @@ class Rammbock(object):
     def delete_interface(self, ifname):
         print "delete_interface " + ifname
         return self.interfaces[ifname].del_interface()
+
+    def delete_all_created_virtual_interfaces(self):
+        for interface in self._virtual_interfaces:
+            interface.del_interface()
+        self._virtual_interfaces.clear()
 
     def start_server(self, if_alias, port):
         self._server.server_startup(if_alias, port)
