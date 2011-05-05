69bcfc14e49ad752aa0000e6894b22ba1d9633bc
Ismo Aro <ismo.aro@gmail.com>
2011-05-05 10:39:26 +0300

    added setup so that client is binding to interface when connecting. hard coded, so nasty one. must fix before push

--- src/rammbock/Client.py
+++ src/rammbock/Client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 #-*- coding: iso-8859-15 -*-
 
-import socket
+import socket, IN
 
 UDP_PACKET_MAX_SIZE = 1024
 
@@ -9,8 +9,9 @@ class Client(object):
 
     def __init__(self):
         self._client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
+                     
     def establish_connection_to_server(self, host, port):
+        self._client_socket.setsockopt(socket.SOL_SOCKET, IN.SO_BINDTODEVICE, 'lo' + '\0')
         self._client_socket.connect((host, int(port)))
 
     def send_packet_over_udp(self, packet): 
