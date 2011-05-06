d1f043dc586332c79bc40072a29084981b610020
Ismo Aro <ismo.aro@gmail.com>
2011-05-06 23:56:04 +0300

    commented out couple of test step from future test cases. left interfaces hanging

--- src/Interface.py
+++ src/Interface.py
@@ -12,14 +12,6 @@ def get_ip_address(ifname):
     output = process.communicate()[0]
     return __return_ip_address_from_ifconfig_output(output)
 
-def __return_ip_address_from_ifconfig_output(output):
-    for line in output.split('\n'):
-        if 'inet addr:' in line:
-            ipAddress = re.compile('addr\:([^\s]+)\s').search(line).group(1)
-            print "ip address is:" + ipAddress
-            return ipAddress
-    return ''
-
 def create_interface_alias(ifname, ip_address, netmask):
     """ Creates interface """
     virtual_if_name = ifname + ":" + str(randint(1000, 10000))
@@ -36,5 +28,14 @@ def check_interface(ifname):
 
 def del_interface(ifname):
     """Deletes this interface"""
+    print "ifconfig", ifname, "down"
     process = subprocess.Popen(["ifconfig", ifname, "down"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     process.wait()
+    
+def __return_ip_address_from_ifconfig_output(output):
+    for line in output.split('\n'):
+        if 'inet addr:' in line:
+            ipAddress = re.compile('addr\:([^\s]+)\s').search(line).group(1)
+            print "ip address is:" + ipAddress
+            return ipAddress
+    return ''
