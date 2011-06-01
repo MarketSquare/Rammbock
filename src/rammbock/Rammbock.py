fb7de94bb1940dbbedd1021617a7aa6f0dfe7e41
Ilmari Kontulainen <kontulai@gmail.com>
2011-06-01 03:17:43 -0500

    Added some tests and implementations to pass tests.

--- src/rammbock/Rammbock.py
+++ src/rammbock/Rammbock.py
@@ -3,11 +3,13 @@ from Server import UDPServer, TCPServer
 import Server
 import Client
 import Encode
-from Message import Message
+from XmlParser import xml2obj
+from os import getcwd
 
 class Rammbock(object):
 
     def __init__(self):
+        self.message = None
         self._servers = {}
         self._clients = {}
 
@@ -60,7 +62,7 @@ class Rammbock(object):
     def server_sends_data(self, packet, name=Server.DEFAULT_NAME): 
         self._servers[name].send_data(packet)
 
-    def client_sends_message(self, message_name, client_name=Client.DEFAULT_NAME, server_name=Server.DEFAULT_NAME):
+    def client_sends_message(self, client_name=Client.DEFAULT_NAME, server_name=Server.DEFAULT_NAME):
         data_bin = Encode.encode_to_bin(self.message)
         self.client_send_data(data_bin)
 
@@ -69,7 +71,20 @@ class Rammbock(object):
         self.version = version
 
     def create_message(self, name):
-        self.message = Message(name)
+        print getcwd()
+        file = open(getcwd() + '/xml/' + name + '.xml')
+        self.message = xml2obj(file)
 
+    def get_header_field(self, name):
+        for hdr in self.message.header:
+            if hdr.name == name:
+                return hdr.data
 
-
+    def get_information_element(self, name):
+        splitted = name.rsplit('.')
+        fetchable = next(x for x in self.message.ie if x.name == splitted[0])
+        for name in splitted[1:]:
+            fetchable = next(x for x in fetchable.ie if x.name == name)
+            if fetchable.name == name:
+                break
+        return fetchable.data
--- src/rammbock/tests/TestMessageTemplate.py
+++ src/rammbock/tests/TestMessageTemplate.py
@@ -1,36 +1,52 @@
 import unittest
 from rammbock import Rammbock
-from rammbock.Message import Message
-from rammbock import Encode
-
+from rammbock.Encode import encode_to_bin
 
 TEST_APPLICATION_PROTOCOL = 'testApplicationProtocol'
 
 class Test(unittest.TestCase):
 
-
     def setUp(self):
-        self._create_example_message()
         self.rammbock = Rammbock()
+        self.message = self._create_example_message()
         self.rammbock.use_application_protocol(TEST_APPLICATION_PROTOCOL)
 
-    def testCreateMessage(self):
-        self.rammbock.create_message('testMessage')
+    def test_create_message(self):
+        self.rammbock.create_message('test')
         actual = self.rammbock.message
         expected = self.message
-        assert(expected.getHeader('protocol'), actual.getheader('protocol'))
-        assert(expected.getIe('message'), actual.getIe('message'))
+        assert expected.header[0].name, actual.header[0].name
+        assert expected.ie[0].name, actual.ie[0].name
 
-    def testEncodeObjectToBin(self):
-        bin_message = Encode.encode_to_bin(self.message)
+    def test_encode_object_to_bin(self):
+        bin_message = encode_to_bin(self.message)
         #TODO: check the size of actual content
-        assert(bin_message, 1234)
-
+        assert bin_message, 1234
 
     def _create_example_message(self):
-        self.message = Message()
-        self.message.addHeader('protocol', TEST_APPLICATION_PROTOCOL, 22)
-        self.message.addIe('message', 'Hello world!', 11)
+        class Object:
+            pass
+
+        class Message(object):
+            header = [None]
+            header[0] = Object()
+            header[0].length = u'22'
+            header[0].name = u'protocol'
+            header[0].data = u'testApplicationProtocol'
+            ie = [None, None]
+            ie[0] = Object()
+            ie[0].length= u'11'
+            ie[0].name= u'message'
+            ie[0].data= u'Hello world!'
+            ie[1] = Object()
+            ie[1].ie = [None]
+            ie[1].ie[0] = Object()
+            ie[1].ie[0].length = u'4'
+            ie[1].ie[0].name= u'test'
+            ie[1].ie[0].data= u'test'
+        return Message()
+
+
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
