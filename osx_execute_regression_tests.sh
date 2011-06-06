98161db6ee279a694fbd69aa54d1328663754da2
Ilmari Kontulainen <kontulai@gmail.com>
2011-06-06 04:54:12 -0500

    and changed osx_execute_regression_tests.sh to use default python instead of python 2.5

--- osx_execute_regression_tests.sh
+++ osx_execute_regression_tests.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 export PATH=$PATH:/usr/local/bin
-
-python2.5 /Library/Python/2.6/site-packages/robotframework-2.5.7-py2.6.egg/robot/runner.py -c regression -L debug --variablefile osx_vars.py --pythonpath src/ atests
+pybot -c regression -L debug --variablefile osx_vars.py --pythonpath src/ atests
