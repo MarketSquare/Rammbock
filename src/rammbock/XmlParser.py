b34a9c04d9ea78a022fbd458dc44fc35ad24c07b
Ismo Aro <ismo.aro@gmail.com>
2011-06-07 15:10:59 +0300

    some refactoring done

--- src/rammbock/XmlParser.py
+++ src/rammbock/XmlParser.py
@@ -6,30 +6,38 @@ class DataNode(object):
     def __init__(self):
         self._attrs = {}    # XML attributes and child elements
         self.data = None    # child text data
+
     def __len__(self):
         # treat single element as a list of 1
         return 1
+
     def __setitem__(self, key, value):
         self._attrs[key] = value
+
     def __getitem__(self, key):
         if isinstance(key, basestring):
             return self._attrs.get(key,None)
         else:
             return [self][key]
+
     def __delattr__(self, key):
         if isinstance(key, basestring):
             del self._attrs[key]
         else:
             del [self][key]
+
     def __contains__(self, name):
         return self._attrs.has_key(name)
+
     def __nonzero__(self):
         return bool(self._attrs or self.data)
+
     def __getattr__(self, name):
         if name.startswith('__'):
             # need to do this for Python special methods???
             raise AttributeError(name)
         return self._attrs.get(name,None)
+
     def _add_xml_attr(self, name, value):
         if name in self._attrs:
             # multiple attribute of the same name are represented by a list
@@ -40,20 +48,25 @@ class DataNode(object):
             children.append(value)
         else:
             self._attrs[name] = value
+ 
     def __str__(self):
         return self.data or ''
+
     def __repr__(self):
         items = sorted(self._attrs.items())
         if self.data:
             items.append(('data', self.data))
         return u'{%s}' % ', '.join([u'%s:%s' % (k,repr(v)) for k,v in items])
 
+
 class TreeBuilder(xml.sax.handler.ContentHandler):
+
     def __init__(self):
         self.stack = []
         self.root = DataNode()
         self.current = self.root
         self.text_parts = []
+
     def startElement(self, name, attrs):
         self.stack.append((self.current, self.text_parts))
         self.current = DataNode()
@@ -61,6 +74,7 @@ class TreeBuilder(xml.sax.handler.ContentHandler):
         # xml attributes --> python attributes
         for k, v in attrs.items():
             self.current._add_xml_attr(_name_mangle(k), v)
+
     def endElement(self, name):
         text = ''.join(self.text_parts).strip()
         if text:
@@ -72,8 +86,10 @@ class TreeBuilder(xml.sax.handler.ContentHandler):
             obj = text or ''
         self.current, self.text_parts = self.stack.pop()
         self.current._add_xml_attr(_name_mangle(name), obj)
+
     def characters(self, content):
         self.text_parts.append(content)
+
 
 def _name_mangle(name):
     return _non_id_char.sub('_', name)
