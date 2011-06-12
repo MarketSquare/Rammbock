3ad817d9da72b83e9d73fc6d6e3254d12481303a
Ismo Aro <ismo.aro@gmail.com>
2011-06-12 21:44:08 +0300

    tried to make more sense to get ies. no luck yet

--- src/rammbock/Decoder.py
+++ src/rammbock/Decoder.py
@@ -1,14 +1,9 @@
 def string2object(message, data):
-    _get_headers(message, data)
-    _get_information_elements(message,data)
-
-def _get_headers(message, data):
     splitted = data.split()
-    _get_headers_from_string(message, splitted)
+    _get_headers_from_string(message, splitted[:3])
+    #_get_ies_from_string(message,splitted[3:])
 
-def _get_information_elements(message, data):
-    splitted = data.split()
-    _get_ies_from_string(message, splitted)
+
 
 def _get_headers_from_string(message, all_headers):
     message.header.append(['Request Method', all_headers[0]])
@@ -16,5 +11,7 @@ def _get_headers_from_string(message, all_headers):
     message.header.append(['Request Version', all_headers[2]])
 
 def _get_ies_from_string(message, splitted):
-    for ie in splitted[2:]:
-        message.ie.append(":".strip(ie))
+    for i in range(len(splitted)):
+        print [":".strip(splitted[i]), splitted[i+1]]
+        message.ie.append[":".strip(splitted[i]), splitted[i+1]]
+        i + 1
