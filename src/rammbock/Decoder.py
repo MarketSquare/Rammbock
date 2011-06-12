e57e841b787c1a07891c4c96e1803fe46f0d9e60
Ismo Aro <ismo.aro@gmail.com>
2011-06-12 21:02:24 +0300

    some skeleton sketched for gettin ies

--- src/rammbock/Decoder.py
+++ src/rammbock/Decoder.py
@@ -1,12 +1,20 @@
 def string2object(message, data):
     _get_headers(message, data)
+    _get_information_elements(message,data)
 
 def _get_headers(message, data):
     splitted = data.split()
     _get_headers_from_string(message, splitted)
 
+def _get_information_elements(message, data):
+    splitted = data.split()
+    _get_ies_from_string(message, splitted)
+
 def _get_headers_from_string(message, all_headers):
     message.header.append(['Request Method', all_headers[0]])
     message.header.append(['Request URI', all_headers[1]])
     message.header.append(['Request Version', all_headers[2]])
-    return message
+
+def _get_ies_from_string(message, splitted):
+    for ie in splitted[2:]:
+        message.ie.append(":".strip(ie))
