a6e778aafadef8990a4fb8157afa94f4109f2c9c
Ismo Aro <ismo.aro@gmail.com>
2011-06-13 07:29:26 +0300

    some renaming done to functions

--- src/rammbock/Decoder.py
+++ src/rammbock/Decoder.py
@@ -1,15 +1,16 @@
 def string2object(message, data):
     splitted = data.split()
-    _get_headers_from_string(message, splitted[:3])
-    _get_ies_from_string(message,splitted[3:])
+    _get_headers_from_list(message, splitted[:3])
+    _get_ies_from_list(message,splitted[3:])
 
-def _get_headers_from_string(message, all_headers):
+def _get_headers_from_list(message, all_headers):
     message.header.append(['Request Method', all_headers[0]])
     message.header.append(['Request URI', all_headers[1]])
     message.header.append(['Request Version', all_headers[2]])
 
-def _get_ies_from_string(message, splitted):
+def _get_ies_from_list(message, splitted):
     for i in range(len(splitted)):
+        #TODO do something about this. it's ugly!
         try:
             message.ie.append([splitted[i][:-1], splitted[i+1]])
         except IndexError:
