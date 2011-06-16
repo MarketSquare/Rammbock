d4c5deff828180e57ab09b8a689a0cdfbfdeb139
Ismo Aro <ismo.aro@gmail.com>
2011-06-16 22:49:34 +0300

    created alternative way to spec received packets

--- src/rammbock/Decoder.py
+++ src/rammbock/Decoder.py
@@ -7,8 +7,9 @@ def string2object(message, data):
 
 def _get_headers_from_list(message, all_headers, headers_schema):
     header_tmp = []
-    header_tmp += [[headers_schema[i], all_headers[i]] for i in range(len(headers_schema))]
+    header_tmp += [[str(headers_schema[i]), all_headers[i]] for i in range(len(headers_schema))]
     message.header = header_tmp
     
 def _get_ies_from_list(message, splitted):
+    print splitted
     message.ie += [[splitted[i][:-1], splitted[i+1]] for i in range(0,len(splitted),2)]
