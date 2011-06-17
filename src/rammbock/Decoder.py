84b957f6a501e36e620c9df88401bced201ece24
Ismo Aro <ismo.aro@gmail.com>
2011-06-17 10:20:59 +0300

    removed some debug prints

--- src/rammbock/Decoder.py
+++ src/rammbock/Decoder.py
@@ -2,13 +2,9 @@ def string2object(message, data):
     splitted = data.split()
     _get_headers_from_list(message, splitted[:len(message.header)], message.header)
     _get_ies_from_list(message,splitted[len(message.header):])
-    print message.header
-    print message.ie
 
 
 def _get_headers_from_list(message, all_headers, headers_schema):
-    print all_headers
-    print headers_schema
     header_tmp = []
     header_tmp += [[str(headers_schema[i]), all_headers[i]] for i in range(len(headers_schema))]
     message.header = header_tmp
