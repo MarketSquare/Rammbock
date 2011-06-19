be144162c710f82cdf0fd85c2efbe7faa53dddeb
Ismo Aro <ismo.aro@gmail.com>
2011-06-20 00:16:51 +0300

    clean

--- src/rammbock/Encoder.py
+++ src/rammbock/Encoder.py
@@ -27,10 +27,3 @@ def _generate_message_from_object(message):
         if j == None:
             whole_message += '\r\n'
             return whole_message
-
-
-def _get_headers_from_msg_object(message):
-    return " ".join(x for _ , x in message.header)
-
-def _get_ie_from_msg_object(message):
-    return "".join(i + ": " + x + "\r\n" for i , x in message.ie)
