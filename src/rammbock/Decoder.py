7d9aebc8e3931814a4a9728fd3c94c4b74895b2d
Ismo Aro <ismo.aro@gmail.com>
2011-06-20 00:41:01 +0300

    everything green again, ugly again, must clean tommorrow

--- src/rammbock/Decoder.py
+++ src/rammbock/Decoder.py
@@ -11,17 +11,20 @@ def string2object(message, data):
 
 def _get_object_from_data(message, data):
     header_value = []
+    tmp_ie_values = []
+    tmp_ie_names = []
     data.reverse()
     for item in message.items:
         if item == 'Header':
             header_value +=  [data.pop()]
         if item ==  'IE':
-            tmp_ie =[]
-            tmp_ie = [data.pop()]
-            print tmp_ie
-            message.ie += tmp_ie
-            foo = data.pop()
 
-    message.header = zip(message.header, header_value)
-    print message.header
+            tmp_ie_name = [data.pop()]
+            tmp_ie = [data.pop()]
+            tmp_ie_values += tmp_ie
+            tmp_ie_names += tmp_ie_name
+    print tmp_ie_names
+    print tmp_ie_values
+    message.ie = zip(message.ie, tmp_ie_values)
     print message.ie
+    message.header = zip(message.header, header_value)
