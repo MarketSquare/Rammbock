eab3887bb487d81dd9c3a3be306fadb89dad59b1
Ismo Aro <ismo.aro@gmail.com>
2011-06-20 15:41:51 +0300

    made encoder little bit nicer

--- src/rammbock/Encoder.py
+++ src/rammbock/Encoder.py
@@ -3,10 +3,6 @@ def object2string(message):
     return whole_message
 
 
-def izip_l(x,y):
-     pad = lambda l1,l2: l1 + [None for _ in range(max(len(l2) -
-len(l1), 0))]
-     return zip(pad(x,y), pad(y,x))
 
 
 def _generate_message_from_object(message):
@@ -14,20 +10,31 @@ def _generate_message_from_object(message):
     message.header.reverse()
     message.ie.reverse()
     for i,j in izip_l(message.items, message.items[1:]):
-        #TODO: this is looking bad. try to do something about it
+    #TODO: this is looking bad. try to do something about it
         if i == 'Header':
-            tmp = message.header.pop()
-            _, x = tmp
-            whole_message += x + " "
+            whole_message += _return_header_from_obj(message)
             if j != 'Header':
                 whole_message += '\r\n'
         elif i == 'IE':
-            tmp = message.ie.pop()
-            tmp = ": ".join(tmp)
-            print "tmp =" +tmp
-            whole_message += tmp + "\r\n"
+            whole_message += _return_ie_from_obj(message)
         else:
             raise Exception(NameError, 'Unwknown type %s' %i)
         if j == None:
             whole_message += '\r\n'
-            return whole_message
+    return whole_message
+
+def izip_l(x,y):
+    pad = lambda l1,l2: l1 + [None for _ in range(max(len(l2) -
+                                                      len(l1), 0))]
+    return zip(pad(x,y), pad(y,x))
+
+def _return_header_from_obj(message):
+    tmp = message.header.pop()
+    _, x = tmp
+    return x + " "
+
+
+def _return_ie_from_obj(message):
+    tmp = message.ie.pop()
+    tmp = ": ".join(tmp)
+    return  tmp + "\r\n"
