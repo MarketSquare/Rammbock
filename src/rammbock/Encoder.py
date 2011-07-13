d4110d636f87a0ab016308128907e7ba0d641274
Ismo Aro <ismo.aro@gmail.com>
2011-07-13 15:19:33 +0300

    removed Decoder.py because it is redundant, also removed unused code from Encoder.py

--- src/rammbock/Encoder.py
+++ src/rammbock/Encoder.py
@@ -1,18 +1,5 @@
 import struct
 
-def object2string(message):
-    whole_message =  _generate_message_from_object(message)
-    return whole_message
-
-def _generate_message_from_object(message):
-    whole_message = ""
-    for index, item in enumerate(message.items):
-        if item['type'] is 'BINARY':
-            whole_message += dec2bin(item['value'], item['length'])
-        elif item['type'] is 'STRING':
-            whole_message += item['value']
-    return whole_message
-
 def dec2bin(data, length):
     data = _add_padding_to_string(data, int(length))
     a = ""
