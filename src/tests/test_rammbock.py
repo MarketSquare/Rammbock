e2eb91d541d2bbab22e56f96f1014e5ab28c2dc1
Ilmari Kontulainen <kontulai@gmail.com>
2011-07-22 06:59:06 -0500

    refactored test case

--- src/tests/test_rammbock.py
+++ src/tests/test_rammbock.py
@@ -10,10 +10,7 @@ class TestNumberFormatting(unittest.TestCase):
         rammbock._binary = ""
 
     def _unpack_bytes_to_hex_string(self):
-        temp = ""
-        for a in rammbock._data:
-            temp += hex(struct.unpack('B', a)[0])[2:].rjust(2, '0')
-        return temp
+        return  "".join([hex(struct.unpack('B', a)[0])[2:].rjust(2, '0') for a in rammbock._data])
 
     def test_add_even_amount_of_numbers_as_tbcd(self):
         rammbock.add_number_as_tbcd('358', '6100000000001')
