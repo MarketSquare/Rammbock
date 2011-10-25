ea1da7900c3e4f561dcd12165da898625ecf8950
Ismo Aro <ismo.aro@gmail.com>
2011-10-25 11:48:53 +0300

    modified documentation

--- src/Rammbock/Rammbock.py
+++ src/Rammbock/Rammbock.py
@@ -333,11 +333,11 @@ class Rammbock(object):
         Adds decimal number as octets. Length as octest can be give. Python base prefixes can be used.
 
         Examples:
-        | Add Decimal as Octets | 42 | 1 | #Adds number 42 as one octet to message. |
-        | Add Decimal as Octets | 42 | 2 | #Adds number 42 as two octets to message. Padding is added |
-        | Add Decimal as Octets | 0x2A | 1 | #Adds number 42 presented as hexadecimal in one octet to message. |
-        | Add Decimal as Octets | 0B00101010 | 1 | #Adds number 42 presented as bit in one octet to message. |
-        | Add Decimal as Octets | 0O52 | 1 | #Adds number 42 presented as octal in one octet to message. |
+        | Add Decimal as Octets | 42 | 1 | |#Adds number 42 as one octet to message. |
+        | Add Decimal as Octets | 42 | 2 | |#Adds number 42 as two octets to message. Padding is added |
+        | Add Decimal as Octets | 2A | 1 | 16 |#Adds number 42 presented as hexadecimal in one octet to message. |
+        | Add Decimal as Octets | 0B00101010 | 1 | 2 | #Adds number 42 presented as bit in one octet to message. |
+        | Add Decimal as Octets | 0O52 | 1 | 8|  #Adds number 42 presented as octal in one octet to message. |
         """
         value = str(int(str(value), int(base)))
         if not int(length):
