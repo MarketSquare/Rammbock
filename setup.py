580d2aaf017672129dae8cac74b4d99cc5ab3e8b
Ilmari Kontulainen <kontulai@gmail.com>
2011-10-28 02:54:28 -0500

    changed setup.py to 0.1.0 version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name         = 'robotframework-rammbock',
-      version      = 'trunk',
+      version      = '0.1.0',
       description  = 'Protocol testing library for Robot Framework',
       author       = 'Robot Framework Developers',
       author_email = 'robotframework-users@googlegroups.com',
