be1b04002e7794ff3baf83f7c9fc9a607894fe37
Ilmari Kontulainen <kontulai@gmail.com>
2012-01-03 08:21:54 -0600

    some protoing

--- proto/Rammbock.py
+++ proto/Rammbock.py
@@ -1,16 +1,25 @@
 # API prototype
 
 class Rammbock(object):
+
+    def __init__(self):
+        self._current_protocol = None
+        self._protocols = {}
+        self._default_client = None
+        self._default_server = None
+
     """Start defining a new protocol template.
 
     All messages sent and received from a connection after calling 'Use protocol' have to use the same protocol template. Protocol template fields can be used to search messages from buffer.
     """
     def start_protocol_description(self, protocol):
-        raise Exception('Not yet done')
+        self._protocols[protocol] = Protocol()
+        self._current_protocol = self._protocols[protocol]
 
     """End protocol definition."""
     def end_protocol_description(self):
-        raise Exception('Not yet done')
+        self._current_protocol.ready = True
+        self._current_protocol = None
 
     def start_udp_server(self, host, ip, _server=None):
         raise Exception('Not yet done')
@@ -26,7 +35,9 @@ class Rammbock(object):
 
     From this moment on (until another protocol is taken into use) all messages received and sent have to confirm to this protocol template."""
     def use_protocol(self, protocol, _server=None, _client=None):
-        raise Exception('Not yet done')
+        self._current_protocol = self._protocols[protocol]
+        self._default_client = _client
+        self._default_server = _server
 
     """Send raw binary data."""
     def send_binary(self, message, _server=None, _client=None, *params):
@@ -54,21 +65,126 @@ class Rammbock(object):
     def receive_pdu(self, *params):
         raise Exception('Not yet done')
 
+    def add_number(self, length, name, value):
+        self._current_protocol.add(_UField(length, name, value))
+
     def u8(self, name, default_value=None):
-        raise Exception('Not yet done')
+        self.add_number(1, name, default_value)
 
     def u16(self, name, default_value=None):
-        raise Exception('Not yet done')
+        self.add_number(2, name, default_value)
 
     def u32(self, name, default_value=None):
-        raise Exception('Not yet done')
+        self.add_number(4, name, default_value)
 
     """Defines the pdu in protocol template.
 
     Length must be the name of a previous field in template definition."""
     def pdu(self, length):
-        raise Exception('Not yet done')
+        self._current_protocol.add(_PDUField(length))
 
     def hex_to_bin(self, hex_value):
         raise Exception('Not yet done')
+
+
+class Protocol(object):
+
+    def __init__(self):
+        self.ready = False
+        self._protocol_template = _Template()
+        self._message_template = _Template()
+
+    def add(self, field):
+        self._add_to_protocol_template(field) if not self.ready else self._add_to_message_template(field)
+
+    def _add_to_protocol_template(self, field):
+        self._protocol_template.add(field)
+
+    def _add_to_message_templte(self, field):
+        self._message_template.add(field)
+
+
+class _Template(object):
+
+    def __init__(self):
+        self._fields = []
+
+    def add(self, to_add):
+        self._fields.append(to_add)
+
+
+class _TemplateField(object):
+
+    def __init__(self, length, name, value):
+        self.name = name
+        self.length = int(length)
+        self.set_value(value)
+
+    def set_value(self, value):
+        self.value = value
+
+    def receive_field_and_validate(self, value, paramdict, big_endian=True):
+        field = self._receive_field(value, big_endian)
+        errors = self._validate(field, paramdict)
+        return field, errors
+
+    def encode_field(self, paramdict):
+        value = self._get_element_value(paramdict)
+        return self._encode_binary_value(value)
+
+    def _receive_field(self, value, big_endian):
+        return value
+
+    def _get_element_value(self, paramdict):
+        return paramdict.get(self.name, self.value)
+
+    def _validate(self, value, paramdict):
+        forced_value = self._get_element_value(paramdict)
+        if not forced_value or forced_value == 'None':
+            return []
+        if forced_value.startswith('('):
+            return self._validate_pattern(forced_value, value)
+        else:
+            return self._validate_exact_match(forced_value, value)
+
+    def _validate_pattern(self, forced_pattern, value):
+        patterns = forced_pattern[1:-1].split('|')
+        for pattern in patterns:
+            if self._is_match(self.name, self.length, pattern, value):
+                return []
+        return ['Value of element %s does not match pattern %s!=%s' %
+                (self.name, to_hex(value), forced_pattern)]
+
+    def _is_match(self, name, length, forced_value, value):
+        forced_binary_val = to_bin_of_length(length, forced_value)
+        return forced_binary_val == value
+
+    def _validate_exact_match(self, forced_value, value):
+        if not self._is_match(self.name, self.length, forced_value, value):
+            return ['Value of element %s does not match %s!=%s' %
+                    (self.name, to_hex(value), forced_value)]
+        return []
+
+
+class _UField(_TemplateField):
+
+    def _receive_field(self, value, big_endian):
+        return self._convert_value_byte_order(value, big_endian)
+
+    def _convert_value_byte_order(self, value, big_endian):
+        return value if big_endian else value[::-1]
+
+    def _encode_binary_value(self, value):
+        return to_bin_of_length(self.length, value)
+
+
+class _StringField(_TemplateField):
+
+    def _encode_binary_value(self, value):
+        return value.ljust(self.length, '\x00')
+
+class _PDUField(object):
+
+    def __init__(self, length):
+        self.length = length
 
