681639251b3bb6fa017a1964e7c05aa552d29943
Jussi Malinen <jussi.malinen@reaktor.fi>
2012-02-08 13:02:00 +0200

    add run script of acceptance tests

---
+++ proto/osx_execute_regression_tests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+base=`dirname $0`
+export PATH=$PATH:/usr/local/bin
+default_target=
+if [ -z "$*" ]
+    then
+      default_target="$base/atest"
+fi
+pybot -c regression -L debug --pythonpath "$base/src/" $default_target "$@"
