5ccb9be17cdb55848477d343afc53848772b896d
Ilmari Kontulainen <kontulai@gmail.com>
2012-02-09 21:15:40 +0200

    Add whitespace between parameters

--- proto/utest/test_rammbock.py
+++ proto/utest/test_rammbock.py
@@ -8,15 +8,15 @@ class TestRammbock(unittest.TestCase):
 
     def test_starting_new_protocol_in_middle_of_old_fails(self):
         self.rammbock.start_protocol_description('foo')
-        self.assertRaises(Exception, self.rammbock.start_protocol_description,'bar')
+        self.assertRaises(Exception, self.rammbock.start_protocol_description, 'bar')
 
     def test_redifining_protocol_fails(self):
         self.rammbock.start_protocol_description('foo')
         self.rammbock.uint(1,'length',None)
         self.rammbock.pdu('length')
         self.rammbock.end_protocol_description()
-        self.assertRaises(Exception, self.rammbock.start_protocol_description,'foo')
+        self.assertRaises(Exception, self.rammbock.start_protocol_description, 'foo')
 
     def test_defining_message_when_defining_protocol(self):
         self.rammbock.start_protocol_description('foo')
-        self.assertRaises(Exception, self.rammbock.new_message,'foo')
+        self.assertRaises(Exception, self.rammbock.new_message, 'foo')
