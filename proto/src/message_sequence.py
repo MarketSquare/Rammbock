4378a4721ecb8f0f2f8b9c168fb7253a5655ce74
Jussi Malinen <jussi.malinen@reaktor.fi>
2012-04-13 14:41:25 +0300

    added license information

--- proto/src/message_sequence.py
+++ proto/src/message_sequence.py
@@ -1,5 +1,20 @@
+#  Copyright 2012 Nokia Siemens Networks Oyj
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 import subprocess
 from OrderedDict import OrderedDict
+
 
 def ip_name(ip, port):
     return '%s:%s' % (ip,port)
@@ -8,6 +23,7 @@ def msg_name(protocol, message_name):
     if protocol and message_name:
         return '%s:%s' % (protocol, message_name)
     return message_name or 'binary'
+
 
 class MessageSequence(object):
 
