3cf4526447c9f87f211914dfc534f433cf563397
Ilmari Kontulainen <kontulai@gmail.com>
2012-05-28 09:54:31 -0500

    modify dns test case and add library to dns

--- atest/dns/DNSLibrary.py
+++ atest/dns/DNSLibrary.py
@@ -1,5 +1,12 @@
 import struct
+from Rammbock.binary_tools import to_0xhex
 
 
 def convert_to_ip(value):
-    return struct.pack('cccc', (number for number in value.split(".")))
+    try:
+        return to_0xhex(struct.pack('BBBB', *(int(number) for number in value.split("."))))
+    except:
+        raise Exception("Malformed IP: %s" % value)
+
+def convert_to_label_sequence(value):
+    return "".join(chr(len(a)) + a for a in value.split('.'))
