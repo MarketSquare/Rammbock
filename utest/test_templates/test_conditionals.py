b9a3357e77ff4f378cfc5101b92e8a293754f95d
Jussi Malinen <jussi.ao.malinen@gmail.com>
2013-02-26 17:13:02 +0200

    moved conditional tests to own file

---
+++ utest/test_templates/test_conditionals.py
@@ -0,0 +1,37 @@
+from unittest import TestCase
+from Rammbock.templates.primitives import UInt, PDU
+from Rammbock.binary_tools import to_bin
+from tools import *
+
+class TestConditional(TestCase):
+
+    def _get_conditional(self):
+        struct = StructTemplate('Foo', 'foo', parent=None)
+        struct.add(UInt(2, 'condition', 1))
+        condition = ConditionalTemplate('condition==1', 'mycondition', None)
+        condition.add(UInt(2, 'myvalue', 42))
+        struct.add(condition)
+        struct.add(UInt(2, 'second', 2))
+        return struct
+
+    def test_condition_is_false(self):
+        cond = self._get_conditional()
+        encoded = cond.encode({'foo.condition': 0}, None)
+        self.assertEquals(encoded.mycondition.exists, False)
+
+    def test_conditional_encode(self):
+        cond = self._get_conditional()
+        encoded = cond.encode({}, None)
+        self.assertEquals(encoded.mycondition.exists, True)
+        self.assertEquals(encoded.mycondition.myvalue.int, 42)
+
+    def test_conditional_decode(self):
+        cond = self._get_conditional()
+        decoded = cond.decode(to_bin('0x00004242'))
+        self.assertEquals(decoded.mycondition.exists, False)
+
+    def test_conditional_decode_has_element(self):
+        cond = self._get_conditional()
+        decoded = cond.decode(to_bin('0x0001 000a 0043'))
+        self.assertEquals(decoded.mycondition.exists, True)
+        self.assertEquals(decoded.mycondition.myvalue.int, 10)
--- utest/test_templates/test_containers.py
+++ utest/test_templates/test_containers.py
@@ -595,39 +595,5 @@ class TestTBCDContainerTemplate(TestCase):
         self.assertEquals(4, len(encoded))
 
 
-class TestConditional(TestCase):
-
-    def _get_conditional(self):
-        struct = StructTemplate('Foo', 'foo', parent=None)
-        struct.add(UInt(2, 'condition', 1))
-        condition = ConditionalTemplate('condition==1', 'mycondition', None)
-        condition.add(UInt(2, 'myvalue', 42))
-        struct.add(condition)
-        struct.add(UInt(2, 'second', 2))
-        return struct
-
-    def test_condition_is_false(self):
-        cond = self._get_conditional()
-        encoded = cond.encode({'foo.condition': 0}, None)
-        self.assertEquals(encoded.mycondition.exists, False)
-
-    def test_conditional_encode(self):
-        cond = self._get_conditional()
-        encoded = cond.encode({}, None)
-        self.assertEquals(encoded.mycondition.exists, True)
-        self.assertEquals(encoded.mycondition.myvalue.int, 42)
-
-    def test_conditional_decode(self):
-        cond = self._get_conditional()
-        decoded = cond.decode(to_bin('0x00004242'))
-        self.assertEquals(decoded.mycondition.exists, False)
-
-    def test_conditional_decode_has_element(self):
-        cond = self._get_conditional()
-        decoded = cond.decode(to_bin('0x0001 000a 0043'))
-        self.assertEquals(decoded.mycondition.exists, True)
-        self.assertEquals(decoded.mycondition.myvalue.int, 10)
-
-
 if __name__ == '__main__':
     main()
