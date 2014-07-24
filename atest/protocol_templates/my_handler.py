96a968b1eddb75e7fc033942403647649e840670
rachanaravindra <travensha@gmail.com>
2014-07-24 12:40:08 +0530

    Tests for responding to the asynchronous message

--- atest/protocol_templates/my_handler.py
+++ atest/protocol_templates/my_handler.py
@@ -1,9 +1,14 @@
 
 RECEIVED_MESSAGES = []
 
+
 def handle_sample(rammbock, msg):
     RECEIVED_MESSAGES.append(msg)
 
+
+def respond_to_sample(rammbock, msg):
+    pass
+
+
 def get_rcvd_msg():
     return RECEIVED_MESSAGES
-
