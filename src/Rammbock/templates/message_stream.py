2b779d2b00ff0d35215841f8e4f87c6f245f566a
rachanaravindra <travensha@gmail.com>
2014-07-24 11:50:14 +0530

    Added support for asynchronous messages

--- src/Rammbock/templates/message_stream.py
+++ src/Rammbock/templates/message_stream.py
@@ -22,6 +22,10 @@ class MessageStream(object):
         self._cache = []
         self._stream = stream
         self._protocol = protocol
+        self._handlers = []
+
+    def set_handler(self, msg_template, handler_func):
+        self._handlers.append((msg_template, handler_func))
 
     def get(self, message_template, timeout=None, header_filter=None):
         header_fields = message_template.header_parameters
@@ -34,7 +38,21 @@ class MessageStream(object):
             header, pdu_bytes = self._protocol.read(self._stream, timeout=timeout)
             if self._matches(header, header_fields, header_filter):
                 return self._to_msg(message_template, header, pdu_bytes)
-            self._cache.append((header, pdu_bytes))
+            else:
+                self._match_or_cache(header, pdu_bytes, header_filter)
+
+    def _match_or_cache(self, header, pdu_bytes, header_filter):
+        for template, func in self._handlers:
+            if self._matches(header, template.header_parameters, header_filter):
+                msg_to_be_sent = self._to_msg(template, header, pdu_bytes)
+                self._get_call_handler(func)(self, msg_to_be_sent)
+                return  #FIXME Make test that handled messages dont go to cache
+        self._cache.append((header, pdu_bytes))
+
+    def _get_call_handler(self,handler_name):
+        module, function = handler_name.split('.')
+        mod = __import__(module)
+        return getattr(mod, function)
 
     def _get_from_cache(self, template, fields, header_filter):
         for index in range(len(self._cache)):
@@ -52,6 +70,7 @@ class MessageStream(object):
         return msg
 
     def _matches(self, header, fields, header_filter):
+        # FIXME: Matching should not be assuming matching string presentation
         if header_filter:
             if header_filter not in fields:
                 raise AssertionError('Trying to filter messages by header field %s, but no value has been set for %s' %
