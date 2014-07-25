6d91e5a70f10c84c12f215a1536f7a0c5df6a83e
rachanaravindra <travensha@gmail.com>
2014-07-25 10:03:40 +0530

    Asynchronous msg handling

--- src/Rammbock/templates/message_stream.py
+++ src/Rammbock/templates/message_stream.py
@@ -45,7 +45,7 @@ class MessageStream(object):
         for template, func in self._handlers:
             if self._matches(header, template.header_parameters, header_filter):
                 msg_to_be_sent = self._to_msg(template, header, pdu_bytes)
-                self._get_call_handler(func)(self, msg_to_be_sent)
+                self._get_call_handler(func)(self._protocol.library, msg_to_be_sent)
                 return  # FIXME Make test that handled messages dont go to cache
         self._cache.append((header, pdu_bytes))
 
